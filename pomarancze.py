35d867b5235ca73b4f88a76a9cc4062a877d5149
hedzio <karolhajduk10@gmail.com>
2024-03-24 16:58:26 +0100

    .

---
+++ pomarancze.py
@@ -0,0 +1,5 @@
+pomaranczowy_krol = 3
+agnieszka = 5
+adam = 6
+pomarancze_razem = (pomaranczowy_krol+agnieszka+adam)
+print(F"Pomarnczowy Król ma {pomaranczowy_krol} pomarancze Agnieszka ma {agnieszka} pomaranczy a Adam ma {adam} pomaranczy. Razem wszyscy maja {pomarancze_razem}Pomaranczy")
