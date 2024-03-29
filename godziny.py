d2ec6a3b5cdee329ca849a70cc8333b3dae4d6a7
hedzio <karolhajduk10@gmail.com>
2024-03-29 23:48:34 +0100

    .

---
+++ godziny.py
@@ -0,0 +1,11 @@
+#Ten program oblicza liczbę sekund w danej liczbie godzin.
+#ten program został napisany dwa dni temu
+
+a = 3 #liczba godzin
+sekundy = 3600 #liczba sekund w 1 godzinie
+
+#print("Godzin: ", a) # wyświetla ilość godzin
+print(F"Jest {a*sekundy} Sekund w {a} godzinach.") # wyswietla ilość sekund w zadanej liczbie godzin
+
+print("Do widzenia!!")
+# to jest koniec programu, który oblicza liczbę sekund w 3 godzinach
