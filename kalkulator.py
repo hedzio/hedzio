cd3df34f29eae7102f2d049337ab3a9a1b06629a
hedzio <karolhajduk10@gmail.com>
2024-03-30 00:03:04 +0100

    .

---
+++ kalkulator.py
@@ -0,0 +1,9 @@
+a = int(input('Podaj wartość "a": '))
+b = int(input('Podaj wartość "b": '))
+
+dodawanie = a + b  
+odejmowanie = a - b
+mnozenie = a * b 
+dzielenie = a / b
+
+print(F"Wynik dodawania to: {dodawanie}, odejmowania: {odejmowanie}, mnożenia {mnozenie}, a dzielenia: {dzielenie}.")
